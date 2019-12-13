dae8858858fc08b3512011df3c50d0b2cb31e130
topazon <topazonzon@gmail.com>
2019-12-13 08:50:58 +0000

    新規投稿実装完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  
+  before_action :set_current_user
+def set_current_user
+  @current_user = User.find_by(id: session[:user_id])
+end
+
+
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  
   def new
     @task = Task.new
   end
@@ -7,7 +8,28 @@ class TasksController < ApplicationController
     @tasks = Task.all.order(created_at: :desc)
   end
   
+  def create
+    @task = Task.new(tasks_params)
+    @task.user_id = @current_user.id
+    
+    
+    if @task.save
+      flash[:success] = "タスク情報を投稿しました。"
+      redirect_to tasks_url
+    else
+      render :new
+    end
+  end
+  
   def show
     @task = Task.find(params[:id])
   end 
+  
+  
+  private
+  
+     
+  def tasks_params
+    params.require(:task).permit(:task_title, :details, :user_id)
+  end
 end
